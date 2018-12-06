e90d371c7df14511ccbbba45ce207d35b9641c4b
刘江 <1433727556@qq.com>
2018-12-06 11:35:18 +0800

    add tool

--- easygameTools/pikachu/vietnam/compare_all_psd_file.rb
+++ easygameTools/pikachu/vietnam/compare_all_psd_file.rb
@@ -1,17 +1,14 @@
 require 'json'
 require 'time'
 require 'digest'
-
 require '../local_config'
 require 'fileutils'
-
 # 对比全部  psd 文件 查询出  没有用过的文件
-
-
 class CompareAllPsdFile
-  attr_accessor :noWenpMap
   # Create the object
-  def initialize(allPsdPath, hasPsdPath ，_result_File)
+  attr_accessor
+
+  def initialize(allPsdPath, hasPsdPath, _result_File)
     # 操作路径,递归层层处理
     @path = allPsdPath
     @hasPsdFile = hasPsdPath
@@ -19,9 +16,7 @@ class CompareAllPsdFile
     @_result_File = _result_File
     @allPPSDFileList = []
     @hasPPSDFileList = []
-    self.begin()
   end
-
 
   def traverse_dir(file_path, allFile = [])
     if File.directory? file_path
@@ -31,7 +26,8 @@ class CompareAllPsdFile
         end
       end
     else
-      puts "File:#{File.basename(file_path)}, Size:#{File.size(file_path)}"
+      allFile.push(file_path)
+      #puts "File:#{File.basename(file_path)}, Size:#{File.size(file_path)}"
     end
     return allFile
   end
@@ -40,28 +36,38 @@ class CompareAllPsdFile
     # 获取 两个文件夹 下所有的 psd 文件
     allFIleList = self.traverse_dir(@path)
     @allPPSDFileList = self.getPsdFileFromAllFile(allFIleList)
-
-
-    hasPSdfileList = self.traverse_dir(@hasPPSDFile)
+    hasPSdfileList = self.traverse_dir(@hasPsdFile)
     @hasPPSDFileList = self.getPsdFileFromAllFile(hasPSdfileList)
-
     @allPPSDFileList.each do |fileName|
-      if not self.chickHasFromList(File.basename(fileName),@hasPPSDFileList)
-
-
+      baseName = "" + fileName
+      if not self.chickHasFromList(File.basename(fileName), @hasPPSDFileList)
+        #_resPath = @_result_File + fileName.sub!(/[#{@path}]/, '')
+        #直接 改一下 即可 实现替换字符串
+        fileName[@path] = ""
+        _resPath = @_result_File + fileName
+        self.copyFile(baseName, _resPath)
       end
-
-
     end
-
-
   end
 
-  def copyFile(beginFile,endFile)
-    FileUtils.cp "#{beginFile}","#{endFile}"
+  def mkdir(dirPath)
+    unless File.exist?(dirPath)
+      self.mkdir(dirPath[0, dirPath.rindex('/')]) if dirPath.rindex('/')
+      Dir::mkdir dirPath
+    end
   end
 
-  def chickHasFromList(fn,list)
+  # 不需要考虑文件夹 是否存在
+  def copyFile(beginFile, endFile)
+    #如果 文件夹  不存在 先创建文件夹  再进行复制
+    dir_name = File.dirname(endFile)
+    self.mkdir(dir_name) unless File.exist?(dir_name)
+    endDir_file = File.dirname(endFile)
+    FileUtils.cp "#{beginFile}", "#{endDir_file}"
+    puts 'cp', "#{beginFile}", "#{endFile}"
+  end
+
+  def chickHasFromList(fn, list)
     list.each do |fileName|
       if File.basename(fileName) == fn
         return true
@@ -79,29 +85,17 @@ class CompareAllPsdFile
       end
     end
     return resList
-
   end
-
-
 end
 
 
 if __FILE__ == $0
   _beginTime = Time.new()
-
-
-  allPsdFis = $svnPath + 'art_pikachu/翻译美术图（英文）/所有的psd '
-
-  allReadyhasPsdPath = '/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/翻译美术（越南）/越南合作方翻译图 '
+  allPsdFis = $svnPath + 'art_pikachu/翻译美术图（英文）/所有的psd'
+  allReadyhasPsdPath = '/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/翻译美术（越南）/越南合作方翻译图'
   #最后生成文件的路径
-  _result_File = ''
-  path = File.absolute_path(ARGV[0])
+  _result_File = '/Users/admin/Desktop/tete/'
   tool = CompareAllPsdFile.new(allPsdFis, allReadyhasPsdPath, _result_File)
-  tool.compressImageToWebp()
-  tool.compressPng()
-  tool.outputNoWebpJson(path)
-  tool.savePngFileLastInfo()
-
-
-  puts "脚本运行时长 => ", Time.new() - _beginTime
+  tool.begin()
+  puts "脚本运行时长 => " + Time.new() - _beginTime
 end
--- easygameTools/test.rb
+++ easygameTools/test.rb
@@ -9,11 +9,16 @@ require 'ftools'
 # puts "save webp map info".split(" ")[0]
 #
 #File.copy '/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/翻译美术图（英文）/所有的psd/繁体翻译art\ pikaqiu/ui道具/fubenruchangjuan.png', '/Users/admin/Desktop/fubenruchangjuan.png'
-discap = '/Users/admin/Desktop/dsad/fds/ds'
-# FileUtils.cp('/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/翻译美术图（英文）/所有的psd/繁体翻译art pikaqiu/ui道具/fubenruchangjuan.png',discap)
+discap = '/Users/admin/Desktop/dsds/'
+#FileUtils.cp('/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/翻译美术图（英文）/所有的psd/art_pikachu俄文版/UI/ani_tuichu.psd',discap)
 #
+fileName = '/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/翻译美术图（英文）/所有的psd/繁体翻译art pikaqiu/ui道具/fubenruchangjuan.png'
+puts fileName
 
-
+sss = 'a'
+puts File.basename(fileName)
+puts File.dirname(fileName)
+puts "fsdafs".sub!(/[#{sss}]/, '######')
 def mkDir(path)
   if !File.directory?(path)
     Dir.mkdir("#{File.expand_path(path)}")#创建tar目录
@@ -21,18 +26,11 @@ def mkDir(path)
   end
 end
 
-mkDir(discap)
-
-def cp(fileName)
-  # if !newDir.empty?
-  #   dst_dir = file_path+"good"
-  #   ds = dst_dir+"/"+newDir
-  #   # FileUtils.mkdir(dst_dir,:mode=>0777) if !File.directory?(dst_dir)
-  #   # FileUtils.mkdir(ds,:mode=>0777) if !File.directory?(ds)
-  #
-  #   FileUtils.mkdir(dst_dir,:mode=>0777) if !File.directory?(dst_dir)
-  #   FileUtils.mkdir(ds,:mode=>0777) if !File.directory?(ds)
-  # end
+ff = "/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/翻译美术图（英文）/所有的psd/art_pikachu俄文版/UI/ani_tuichu.psd"
+dd = "/Users/admin/Documents/ljworkspace/local/cocos/assets/pikachu/sanguo/art_pikachu/翻译美术图（英文）/所有的psd"
 
 
-end
+#ff.gsub(/[#{dd}]/, '')
+ff[dd]=""
+puts ff
+
