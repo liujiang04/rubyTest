4645ecb7f7f8f4e097085cbc76188223623ae586
刘江 <1433727556@qq.com>
2018-12-26 20:54:26 +0800

    add html

--- rails_demo/app/controllers/welcome_controller.rb
+++ rails_demo/app/controllers/welcome_controller.rb
@@ -1,4 +1,21 @@
 class WelcomeController < ApplicationController
   def index
+
+  end
+
+
+  def post_txt
+    @post = Post.new
+    respond_to do |format|
+      format.html
+      format.json { render json:{} }
+      format.js
+    end
+
+    #
+    # if params[:email]
+    #   render :text=>"false"
+    # end
+    # render :text=>"false"
   end
 end
